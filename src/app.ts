6710239c3463314c58d7eab509fb8568d8ac0f1d
Bulbul Ahammed Riad <bulbulprograming@gmail.com>
2024-10-28 00:26:20 +0600

    some change

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,16 @@ const port = 3000
 import cors from 'cors';
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin : ['http://localhost:3000'],
+  credentials : true,
+  optionsSuccessStatus : 200,
+  methods : ['GET', 'POST', 'PUT', 'DELETE']
+}))
 
 
 app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
+  res.send('Server is running')
 })
 
 export default app
